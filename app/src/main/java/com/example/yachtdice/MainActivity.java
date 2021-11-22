b335c9ed6bcabe0271eae355013b939b1b642327
Kang <rkdxorud3939@naver.com>
2021-11-22 22:59:39 +0900

    first commit

---
+++ app/src/main/java/com/example/yachtdice/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.yachtdice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+    private EditText et_test;
+    private String str;
+    private ImageView test;
+    private ListView list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et_test = findViewById(R.id.et_test);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str = et_test.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str", str );
+                startActivity(intent);
+            }
+        });
+
+        test = findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"주사위를 굴렸습니다", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        list = findViewById(R.id.list);
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,data);
+        list.setAdapter(adapter);
+        data.add("1");
+        data.add("2");
+        data.add("3");
+        data.add("4");
+        data.add("5");
+        data.add("6");
+        adapter.notifyDataSetChanged();
+    }
+}
